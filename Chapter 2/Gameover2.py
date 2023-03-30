214711220307aacfe63b0a39890e0ffcc6ad0694
MalwareDetectedBlog <MalwareDetected0@gmail.com>
2023-03-30 16:49:07 +0100

    Completed pages 1-22, understand how to use the backslash key in many different use cases from breaking lines to making quotes

---
+++ Chapter 2/Gameover2.py
@@ -0,0 +1,12 @@
+# Game over 2.0
+
+print("Program 'Game Over' 2.0")
+print("Same", "message", "as before")
+print("Just",
+      "a bit",
+      "bigger")
+print("Here", end=" ")
+print("it is...")
+print("""Game over""")
+input("\n\n Press the enter key to exit.")
+
