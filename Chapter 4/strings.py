b084489012d99727fefd0b10a0939235a63b690e
MalwareDetectedBlog <MalwareDetected0@gmail.com>
2023-04-12 16:06:12 +0100

    I understand string immutability

--- Chapter 4/strings.py
+++ Chapter 4/strings.py
@@ -1,4 +1,9 @@
-message = input("Enter a message")
+import random
+
+message = input("Enter a message: ")
+new_message = ""
+VOWELS = "aeiou"
+
 print("The message length:", len(message))
 
 if "e" in message:
@@ -6,4 +11,18 @@ if "e" in message:
 else:
     print("is not in your message")
 
-    
+word = "index"
+
+high = len(word)
+low = -len(word)
+
+for i in range (10):
+    position = random.randrange(low, high)
+    print("word[",position, "]\t", word[position])
+
+for letter in message:
+    if letter.lower() not in VOWELS:
+        new_message += letter
+        print("A new string has been created:",new_message)
+
+print("Without vowels:",new_message)
